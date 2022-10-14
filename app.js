4b2d81378e75ea872a8c4eca4ae0a79c9dd247e7
mcsel <marcel.selle@sms-group.com>
2022-10-14 16:06:12 +0200

    radio input and script to read checked value

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const cardThanks = document.getElementById('cardThanks');
 
 const submitButton = document.getElementById('submitButton');
 
+//const rating = document.querySelector('input[name="rating"]:checked').value
+
+
+
 submitButton.onclick = () => {
   cardRating.classList.add('hidden')
   cardThanks.classList.remove('hidden')
